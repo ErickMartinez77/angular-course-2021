862a568dffbbf18d683c87cef6543848a093d722
ErickMartinez77 <erickmartinezhuaylla123@gmail.com>
2021-08-09 17:39:20 -0400

    08/09/2010 creation of second component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {Test1Component} from './test1/test1.component'
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Test1Component
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Test1Component, Test2Component],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'erick-comp',
   templateUrl: './test1.component.html',
-  styleUrls: ['./test1.component.css']
+  styleUrls: ['./test1.component.scss']
 })
 export class Test1Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
