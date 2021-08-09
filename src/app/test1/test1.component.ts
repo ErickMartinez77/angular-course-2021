ff979f484c8789ac5670c376ccd96e89de3789d1
ErickMartinez77 <erickmartinezhuaylla123@gmail.com>
2021-08-09 17:51:19 -0400

    08/09/2021 my first input in a component

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'erick-comp',
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.scss']
 })
-export class Test1Component implements OnInit {
-  constructor() {}
+export class Test1Component {
+  @Input('status') statusTest: string = 'working';
 
-  ngOnInit() {}
+  constructor() {}
 }
