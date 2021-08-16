0ec69d1f7b22fc7f2ecfebe6c65b1188304501e8
ErickMartinez77 <erickmartinezhuaylla123@gmail.com>
2021-08-16 17:44:25 -0400

    16/08/2021 create first attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { FormsModule } from '@angular/forms';
 import { Test3Component } from './test3/test3.component';
+import { Directive1Directive } from './directive1.directive';
 
 @NgModule({
   declarations: [
     AppComponent, 
     Test1Component, 
     Test2Component,
-    Test3Component
+    Test3Component,
+    Directive1Directive
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directive1.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+
+@Directive({
+  // patron decorator
+  selector: '[directive1Test]'
+})
+export class Directive1Directive {
+
+  @Input() color:string = 'yellow';
+  @Output() outputTest = new EventEmitter<any>(null);
+
+  @HostListener('click') onClick(){
+    this.setBackgroundColor(this.color);
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.setBackgroundColor('green');
+    this.outputTest.emit('test myoutput');
+  }
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.setBackgroundColor('gray');
+  }
+
+  constructor(private element: ElementRef) {//ElementRef hace refe a culquier elemento html
+    this.setBackgroundColor(this.color);
+    this.element.nativeElement.style.border = '10px green solid';  
+  }
+
+  setBackgroundColor(color:string){
+    this.element.nativeElement.style.backgroundColor = color
+  }
+}
