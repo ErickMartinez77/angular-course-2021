07d2ca785654418f5d278836bcb14f0ec0327533
ErickMartinez77 <erickmartinezhuaylla123@gmail.com>
2021-08-18 17:40:48 -0400

    18/08/2021 task modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { Test3Component } from './test3/test3.component';
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
 import { Module1Module } from './module1/module1.module';
+import { HomeModule } from './pages/home/home.module';
+import { AdminModule } from './pages/admin/admin.module';
+import { BuyModule } from './pages/buy/buy.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { Module1Module } from './module1/module1.module';
   imports: [
     BrowserModule,
     FormsModule,
-    Module1Module
+    HomeModule,
+    AdminModule,
+    BuyModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
